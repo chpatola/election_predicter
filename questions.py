987cd36c1b9cfb336042484c32df53e3a6bb7781
chpatola <charlotte.patola@helsinki.fi>
2019-10-22 10:55:03 +0300

    tiedied up the code in election machine

---
+++ questions.py
@@ -0,0 +1,48 @@
+
+#Questions for predictions in election machine
+
+ageQ = "Age in years, for example 32: "
+sexQ ="Sex: female = 0, man = 1 : "
+celebQ ="Celebrity: 0 = no, 1 = yes: "
+parliamQ="Currently in parliament: 0 = no, 1 = yes: "
+motherTQ="""Mother toungue:
+other than finnish or swedish = 0
+swedish = 1
+finnish = 2: """
+langQ="Number of languages candidate knows: "
+webQ="Has twitter : 0 = no, 1 = yes: "
+childrenQ="Has children: no =0, yes = 1: "
+employerQ="""Employer:
+not working = 0
+public employer = 1
+other than public, private or not working = 2
+private employer = 3: """
+educQ = """Education level:
+0 = elementary school
+1 = vocational school or similar
+2 = university / university of applied sciences
+3 = other: """
+workQ="""Work status: expert/public officer/higly educated = 0
+pensioner = 1
+management/leading position = 2
+other than included on this list = 3
+farmer = 4
+student = 5
+artist = 6
+worker = 7
+entreprenour = 8: """
+pmPartyQ="Member in current prime minister party, 0 = no, 1 = yes: "
+extFundQ="Has external funding for election, 0 = no, 1 = yes: "
+electBudjQ="""Election budget:
+<1k € = 0
+1-5k € = 1
+5-10 k € = 2
+10-20k € =3
+20-50k € = 4
+>50k € = 5: """
+yIncomeQ="""Yearly income: <20k € = 0
+20-30k € = 1
+30-50k € = 2
+50-70k € = 3
+70-100k € = 4
+>100k € = 5: """
